5f22605335819b8c71939c18e7c0c362ba78f498
bethclarke-s <beth.clarke@softwire.com>
2026-09-08 16:49:13 +0100

    Refactoring

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,23 @@
 // This is our main function
 function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 ===0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0){
-            console.log("Fizz");
-        } else if (i % 5 === 0){
-            console.log("Buzz");
-        } else {
-            console.log(i);
+
+        let string = "";
+
+        if (i % 3 ===0) {
+            string += "Fizz";
+        } 
+        
+        if (i % 5 === 0){
+            string += "Buzz";
         }
+
+        if (string === "") {
+            string += i.toString();
+        }
+
+        console.log(string);
+
     }
 }
 
